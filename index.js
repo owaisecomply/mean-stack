8517ce6c438cc18863f611b480630aa2b71b0920
owaisecomply <owais@ecomply>
2017-12-15 19:11:54 +0500

    fix port

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ app.get('*', (req, res)=> {
   res.sendFile(path.join(__dirname, 'public/index.html'))
 })
 
-const PORT =process.env.port || 3000;
+const PORT =process.env.PORT || 3000;
 app.listen( PORT, ()=>{
     console.log(`Server is on localhost:${PORT}`)
 })
